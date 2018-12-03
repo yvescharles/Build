6446265767d2f4700429ac58abe1f3035bb799eb
yvescharles <yc@avioneo.com>
2018-12-03 18:11:30 +0100

    modif sh

--- auto-reset-Px4-fork.sh
+++ auto-reset-Px4-fork.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 clear
-message='
------------------------------------\n
-- script de compilation auto PX4  -\n
------------------------------------\n'
+daten=`date +%y%m%d-%H:%M:%S`
+message="
+----------------------------------------------------------------------------------\n
+--     script de compilation auto PX4 v$daten  --\n
+----------------------------------------------------------------------------------\n"
 echo -e $message
 
-
-nom="v1.8.2"
-#git checkout $nom
-#git branch
-#git status
-#git pull origin $nom
+nom="v_$daten"
 
 nomcompil="compilation_$nom"
-dir="~/root/Build/$nomcompil"
+dir="/root/Build/$nomcompil"
 log="$dir/log.txt"
+git branch -D $nomcompil
 git checkout -b $nomcompil
 mkdir -p $dir
-git branch
-sleep 5
 
-cd ~/src/Firmware
-sudo ./Tools/docker_run.sh 'make clean'
-sudo ./Tools/docker_run.sh 'make px4_fmu-v3_default'
-cp ~/src/Firmware/build/px4_fmu-v3_default/px4_fmu-v3_default.px4 $dir/px4_fmu-v3_default.px4
+cd /root/src/Firmware
+#sudo ./Tools/docker_run.sh 'make clean'
+#sudo ./Tools/docker_run.sh 'make px4_fmu-v3_default'
+cp /root/src/Firmware/build/px4_fmu-v3_default/px4_fmu-v3_default.px4 $dir/px4_fmu-v3_default.px4
 
+cd $dir
 git branch
 git status
+git add $dir
+git commit -m "push auto $nom"
+git push origin $nomcompil
 
