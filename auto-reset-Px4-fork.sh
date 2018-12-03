90d70aa7510adfca97c2223404e0f58c4af6dea3
Yves CHARLES <44416062+yvescharles@users.noreply.github.com>
2018-12-03 17:11:21 +0100

    test

--- auto-reset-Px4-fork.sh
+++ auto-reset-Px4-fork.sh
@@ -6,23 +6,7 @@ message='
 -----------------------------------\n'
 echo -e $message
 
-cd ~/
-rm -r ~/src
-mkdir ~/src
-sleep 1
-cd ~/src/
-git clone https://github.com/yvescharles/Firmware.git
-cd ~/src/Firmware/
-git remote add original https://github.com/PX4/Firmware.git
-git remote -v
-git fetch original
-git checkout master
-git reset --hard original/master
-git push origin master --force
-git branch
-git status
-#git tag -l
-#read -p 'Entrez le numéro de version à compiler (12 car  max) : ' -n 12 -t 25 nom
+
 nom="v1.8.2"
 #git checkout $nom
 #git branch
